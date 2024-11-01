e15eabd0477f402010905e02811506ab32e951e6
ju-lit-hein <julien.ferdinand@epitech.eu>
2024-11-02 01:48:14 +0400

    feat: Enhanced map generation

--- scripts/generation.gd
+++ scripts/generation.gd
@@ -1,9 +1,9 @@
 extends Node
 
 @export var GENERATION_BOUND_DISTANCE = 8
-@export var VERTICAL_AMPLITUDE = 10
+@export var VERTICAL_AMPLITUDE = 1
 
-var noise = FastNoiseLite.new()
+@export var noise : FastNoiseLite
 var generated_chunks
 
 @onready var city = preload("res://World/blue_city.tscn")
