523e7854eca62832823c647b847ca281ce07d3f8
ju-lit-hein <julien.ferdinand@epitech.eu>
2024-11-02 17:52:59 +0400

    feat: remove current enemy movement to adopt the real one when merging

--- enemy.gd
+++ enemy.gd
@@ -1,12 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 1.0
 const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta: float) -> void:
-
-	velocity.x = move_toward(velocity.x, 0, SPEED)
-	velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+	pass
--- test_map.gd
+++ test_map.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var NSPAWN = 2
 @export var maxX = 10
 @export var maxZ = 10
-@onready var spawns = [Vector2(randi() % maxX, randi() % maxZ) * NSPAWN]
+@onready var spawns = [Vector3(randi() % maxX, 0, randi() % maxZ) * NSPAWN]
 #change with the navigation region
 @onready var navigation_region = 0
 
@@ -19,9 +19,9 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_spawn_timer_timeout() -> void:
+	randomize()
 	var spawn_point = spawns.pick_random()
 	instance = enemy.instantiate()
 	instance.set("position", spawn_point)
 	print("1 enemy spawned")
 	add_child(instance)
-	
